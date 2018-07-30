cb5026464530d03c70df1e6892ef37cef6618d42
Timothy Ege <retibra@gmail.com>
2018-07-30 14:13:49 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -7,28 +7,13 @@ class Application
     req = Rack::Request.new(env)
 
     if req.path.match(/items/)
-      @@items.each do |item|
-        resp.write "#{item}\n"
-      end
-    elsif req.path.match(/search/)
-      search_term = req.params["q"]
-      resp.write handle_search(search_term)
-    elsif req.path.match(/cart/)
-      if @@cart.empty?
-        resp.write "Your cart is empty"
+      item_name = req.path.split("/items/").last
+      if @@items.includes? item_name 
+        resp.write item_name.price
       else
-        @@cart.each do |cart|
-          resp.write "#{cart}\n"
-        end
+        resp.status = 400
+        resp.write "Item not found"
       end
-    elsif req.path.match(/add/)
-      item_to_add = req.params["item"]
-        if @@items.include? item_to_add
-          @@cart << item_to_add
-          resp.write "added #{item_to_add}"
-        else
-          resp.write "We don't have that item"
-        end
     else
       resp.write "Route not found"
       resp.status = 404
