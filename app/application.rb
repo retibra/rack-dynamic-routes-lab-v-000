cbe571ddf03cc1b5c3efea4f3f9e273c7e7c477d
Timothy Ege <retibra@gmail.com>
2018-07-30 14:13:58 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -8,7 +8,7 @@ class Application
 
     if req.path.match(/items/)
       item_name = req.path.split("/items/").last
-      if @@items.includes? item_name
+      if @@items.include? item_name
         resp.write item_name.price
       else
         resp.status = 400
